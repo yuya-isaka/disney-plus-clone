2bfa6b7ea81dc14366f1e9d000fe31ce8c06ddfe
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-05-14 18:39:23 +0900

    add Header nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
+            <Header />
             <Login />
           </Route>
         </Switch>
---
+++ src/components/Header.js
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+const Header = (props) => {
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt="" />
+      </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src="/images/home-icon.svg" alt="Home" />
+          <span>HOME</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  max-width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  width: 100%;
+`;
+
+const NavMenu = styled.div`
+  flex-wrap: nowrap;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      white-space: nowrap;
+      margin-left: 6px;
+      margin-top: 3px;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        left: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+export default Header;
